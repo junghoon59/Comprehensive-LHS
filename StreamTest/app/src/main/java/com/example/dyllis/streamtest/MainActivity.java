e3c9bfbe2fbca120959f0a02496e416ac06e5398
Hyunsoo Lee <forusers12@gmail.com>
2018-06-10 04:24:59 +0900

    [버전] 이미지 다운 후 보여주기

--- StreamTest/app/src/main/java/com/example/dyllis/streamtest/MainActivity.java
+++ StreamTest/app/src/main/java/com/example/dyllis/streamtest/MainActivity.java
@@ -1,56 +1,99 @@
 package com.example.dyllis.streamtest;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.dyllis.streamtest.R;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView txtRead;
-    final static String filePath = Environment.getExternalStorageDirectory().getAbsolutePath()+"/fff.txt";
 
+    Button mDown;
+    ImageView mImgTrans;
+    Bitmap mBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDown = (Button) findViewById(R.id.btn_down);
+        mImgTrans = (ImageView) findViewById(R.id.imgTranslate);
 
-        txtRead = (TextView)findViewById(R.id.txtRead);
-    }
+        mDown.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                new LoadImage()
+                        .execute("https://t1.daumcdn.net/cfile/tistory/212DAC4C553978792D");
 
-    public void mOnFileRead(View v){
-        String read = ReadTextFile(filePath);
-        txtRead.setText(read);
-    }
-
-
-    //경로의 텍스트 파일읽기
-    public String ReadTextFile(String path){
-        StringBuffer strBuffer = new StringBuffer();
-        try{
-            InputStream is = new FileInputStream(path);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-            String line="";
-            while((line=reader.readLine())!=null){
-                strBuffer.append(line+"\n");
             }
+        });
+    }
 
-            reader.close();
-            is.close();
-        }catch (IOException e){
-            e.printStackTrace();
-            return "";
+    private class LoadImage extends AsyncTask<String, String, Bitmap> {
+
+        ProgressDialog pDialog;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(MainActivity.this);
+            pDialog.setMessage("이미지 로딩중입니다...");
+            pDialog.show();
         }
-        return strBuffer.toString();
+
+        protected Bitmap doInBackground(String... args) {
+            try {
+                mBitmap = BitmapFactory
+                        .decodeStream((InputStream) new URL(args[0])
+                                .getContent());
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return mBitmap;
+        }
+
+        protected void onPostExecute(Bitmap image) {
+
+            if (image != null) {
+                mImgTrans.setImageBitmap(image);
+                pDialog.dismiss();
+
+            } else {
+                pDialog.dismiss();
+                Toast.makeText(MainActivity.this, "이미지가 존재하지 않습니다.",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        }
     }
 }
+
 
